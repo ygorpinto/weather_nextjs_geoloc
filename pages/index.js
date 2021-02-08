30bc94a092c19060cfc3604bb868344e018e226b
Ygor Ferreira <ygor.pinto@hotmail.com>
2021-02-08 13:52:45 -0300

    ok

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
 import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import {useState, useEffect} from 'react'
 
 export default function Home() {
-  return <Title>My page</Title>
+
+const [weather, setweather] = useState({})
+
+  useEffect(function getLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(position=>
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=9bea4fffe3d51d2da210f4269fa3a525`)
+        .then(response=>response.json())
+        .then(data=>setweather(data))
+        )
+      }
+    })
+
+  console.log(weather);
+
+  return (
+    <>
+    <p>A Temperatuda de {weather.name} é {weather.main.temp}ºC</p>
+    </>
+  )
 }
