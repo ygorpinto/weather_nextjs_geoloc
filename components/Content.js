5615cec8bc47d9abe905dc614cbb2e079f2db512
Ygor Ferreira <ygor.pinto@hotmail.com>
2021-02-08 21:31:24 -0300

    all

---
+++ components/Content.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Content = ({wname,wtemp})=>{
+return (
+<div>
+<p>{wname}</p>
+<p>{wtemp}</p>
+</div>
+)
+}
+
+export default Content
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,33 @@
 import styled from 'styled-components'
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
+import Content from '../components/Content'
+import { typeOf } from 'react-is';
 
 export default function Home() {
-
 const [weather, setweather] = useState({})
-
+console.log(weather);
   useEffect(function getLocation() {
     if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition(position=>
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=9bea4fffe3d51d2da210f4269fa3a525`)
+        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=9bea4fffe3d51d2da210f4269fa3a525`)
         .then(response=>response.json())
         .then(data=>setweather(data))
         )
       }
     })
+  
   return (
     <>
-    <p>A Temperatuda de {weather.name} é {weather.main.temp}ºC</p>
+    {(typeof weather.main != "undefined") ? (
+    <Content 
+    wname={weather.name}
+    wtemp={weather.main.temp}
+    >
+    </Content>
+    ) : ('')}
     </>
+    
   )
 }
+
+
