e25a9564c614567d125086c197bda49735ca4ec7
Ygor Ferreira <ygor.pinto@hotmail.com>
2021-02-08 21:33:48 -0300

    ok

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ console.log(weather);
   useEffect(function getLocation() {
     if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition(position=>
-        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=9bea4fffe3d51d2da210f4269fa3a525`)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=9bea4fffe3d51d2da210f4269fa3a525`)
         .then(response=>response.json())
         .then(data=>setweather(data))
         )
